ce5b80d169f747a131981facbfc4c90c086f8505
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2024-12-16 00:19:34 -0800

    Registered callbacks (committing small change before being done for the night)

--- game/main.zig
+++ game/main.zig
@@ -4,12 +4,6 @@
 //  Description: 
 //  	** Holds the main entry pointer for the game portion of the codebase
 
-/// Default GLFW error handling callback
-fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void
-{
-	std.log.err("glfw: {}: {s}\n", .{ error_code, description });
-}
-
 const Window = struct
 {
 	allocator: Allocator,
@@ -48,6 +42,60 @@ const Window = struct
 	}
 };
 
+/// Default GLFW error handling callback
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void
+{
+	std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+}
+
+fn posCallback(window: glfw.Window, xpos: i32, ypos: i32) void
+{
+	_ = window;
+	_ = xpos;
+	_ = ypos;
+}
+fn sizeCallback(window: glfw.Window, width: i32, height: i32) void
+{
+	_ = window;
+	_ = width;
+	_ = height;
+}
+fn closeCallback(window: glfw.Window) void
+{
+	_ = window;
+}
+fn refreshCallback(window: glfw.Window) void
+{
+	_ = window;
+}
+fn focusCallback(window: glfw.Window, focused: bool) void
+{
+	_ = window;
+	_ = focused;
+}
+fn iconifyCallback(window: glfw.Window, iconified: bool) void
+{
+	_ = window;
+	_ = iconified;
+}
+fn maximizeCallback(window: glfw.Window, maximized: bool) void
+{
+	_ = window;
+	_ = maximized;
+}
+fn framebufferSizeCallback(window: glfw.Window, width: u32, height: u32) void
+{
+	_ = window;
+	_ = width;
+	_ = height;
+}
+fn contentScaleCallback(window: glfw.Window, xscale: f32, yscale: f32) void
+{
+	_ = window;
+	_ = xscale;
+	_ = yscale;
+}
+
 pub fn main() !void
 {
 	var stdGpa = std.heap.GeneralPurposeAllocator(.{
@@ -56,7 +104,11 @@ pub fn main() !void
 		.thread_safe = false,
 		.verbose_log = false,
 	}){};
-	defer { const deinitResult = stdGpa.deinit(); assert(deinitResult == .ok); }
+	defer
+	{
+		const deinitResult = stdGpa.deinit();
+		assert(deinitResult == .ok);
+	}
 	const stdAllocator = stdGpa.allocator();
 	
 	glfw.setErrorCallback(errorCallback);
@@ -75,7 +127,18 @@ pub fn main() !void
 	};
 	var window = Window { .allocator = stdAllocator, .glfwWindow = glfwWindow };
 	defer window.destroy();
+	
 	try window.setName("Zig Game!");
+	
+	window.glfwWindow.setPosCallback(posCallback);
+	window.glfwWindow.setSizeCallback(sizeCallback);
+	window.glfwWindow.setCloseCallback(closeCallback);
+	window.glfwWindow.setRefreshCallback(refreshCallback);
+	window.glfwWindow.setFocusCallback(focusCallback);
+	window.glfwWindow.setIconifyCallback(iconifyCallback);
+	window.glfwWindow.setMaximizeCallback(maximizeCallback);
+	window.glfwWindow.setFramebufferSizeCallback(framebufferSizeCallback);
+	window.glfwWindow.setContentScaleCallback(contentScaleCallback);
 	
 	// Wait for the user to close the window.
 	while (!window.glfwWindow.shouldClose())
